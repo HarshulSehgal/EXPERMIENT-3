0a93374c0e5c35ad75b2d27f26a5ad32fa80b8ad
HarshulSehgal <harshulcu@gmail.com>
2026-02-13 16:22:54 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/FS-1-Exp3/"   // for GitHub Pages
-      : "/",            // for local + Netlify dev
 });
